d2dbf8638f9d9b7f49f851bae9dfe74c27568f17
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-02-26 02:24:54 +0300

    Added material design.

---
+++ yakov.OOP.Drawing/yakov.OOP.Drawing.Model/DrawingTools/Figures/FigureBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace yakov.OOP.Drawing.Model.DrawingTools.Figures
+{
+    public class FigureBase
+    {
+        public Point MouseLeftDownPos { get; set; }
+        
+        public Point MouseLeftUpPos { get; set; }
+    }
+}
--- yakov.OOP.Drawing/yakov.OOP.Drawing.ViewModel/MainContext.cs
+++ yakov.OOP.Drawing/yakov.OOP.Drawing.ViewModel/MainContext.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace yakov.OOP.Drawing.ViewModel
 {
-    internal class MainContext: INotifyPropertyChanged
+    public class MainContext : INotifyPropertyChanged
     {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
