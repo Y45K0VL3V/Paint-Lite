e3de2b0dfca553b35e8284b3609cb185dc37aabf
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-02-28 02:39:09 +0300

    Added choosing tool binding. Determine tool type.

--- yakov.OOP.Drawing/yakov.OOP.Drawing.ViewModel/MainContext.cs
+++ yakov.OOP.Drawing/yakov.OOP.Drawing.ViewModel/MainContext.cs
@@ -5,27 +5,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Media;
+using yakov.OOP.Drawing.Model;
 using yakov.OOP.Drawing.Model.DrawingTools;
 using yakov.OOP.Drawing.Model.DrawingTools.Figures;
 
 namespace yakov.OOP.Drawing.ViewModel
 {
+    public enum ToolType
+    {
+        Pen,
+        Line,
+        Rect,
+        RectRounded,
+        Square,
+        Ellipse,
+        Circle
+    }
     public class MainContext : INotifyPropertyChanged
     {
-        // Contain currently used drawing tool.
-        private object _usingTool;
-        public object UsingTool
+        // Get tool type by index.
+        // Index - selected item in view.
+        private ToolType? GetToolByIndex(int? index)
         {
-            get { return _usingTool; }  
+            if (index == null) return null;
+
+            for (ToolType toolType = ToolType.Pen; toolType <= ToolType.Circle; toolType++)
+            {
+                if ((int)toolType == index)
+                {
+                    return toolType;
+                }
+            }
+
+            return null;
+        }
+
+        // Contain currently used drawing tool.
+        private ToolType? _usingTool;
+        public int? UsingTool
+        {
+            get 
+            { 
+                if (_usingTool != null) 
+                    return (int)_usingTool; 
+                else 
+                    return null; 
+            }  
             set 
             { 
-                _usingTool = value;
+                _usingTool = GetToolByIndex(value);
                 OnPropertyChanged("UsingTool");
             }
         }
 
         public Color UsingColor { get; set; }
+
+        private RelayCommand com;
+        public RelayCommand Com
+        {
+            get
+            {
+                return com ?? (com = new RelayCommand(obj =>
+                {
+                    
+                }));
+            }
+        }
 
         // INotifyPropertyChanged realisation.
         public event PropertyChangedEventHandler PropertyChanged;
